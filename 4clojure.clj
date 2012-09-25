8a522766763e18762ce096eaab5142ccad018529
Jim Menard <jim@jimmenard.com>
2012-09-25 07:16:21 -0400

    randomness avoids hanging up in dfa, but it takes too long to run

--- 4clojure.clj
+++ 4clojure.clj
@@ -3094,71 +3094,82 @@ symbols are in the alphabet #{'a, 'A, 'b, 'B, ...}."
                (get (get (:transitions dfa) curr-state) sym))
               (build-accepted
                [prefix state]
-               (for [s (:alphabet dfa)
+               (for [s (shuffle (:alphabet dfa))
                      :let [new-s (str prefix s)
                            new-state (next-state s state)]]
                  (cond (accepted? new-state) (lazy-seq (conj (build-accepted new-s new-state) new-s))
                        new-state (lazy-seq (build-accepted new-s new-state))
-                       :else nil)))]
+                       :else nil)))
+              (generate-accepted
+               []
+               (dosync (ref-set accepted #{}))
+               (build-accepted "" (:start dfa)))]
         (filter identity (flatten (build-accepted "" (:start dfa)))))))
 )
 
-(and
- (= #{"a" "ab" "abc"}
-    (set (__ '{:states #{q0 q1 q2 q3}
-               :alphabet #{a b c}
-               :start q0
-               :accepts #{q1 q2 q3}
-               :transitions {q0 {a q1}
-                             q1 {b q2}
-                             q2 {c q3}}})))
- (= #{"hi" "hey" "hello"}
-    (set (__ '{:states #{q0 q1 q2 q3 q4 q5 q6 q7}
-               :alphabet #{e h i l o y}
-               :start q0
-               :accepts #{q2 q4 q7}
-               :transitions {q0 {h q1}
+(def dfa1 '{:states #{q0 q1 q2 q3}
+            :alphabet #{a b c}
+            :start q0
+            :accepts #{q1 q2 q3}
+            :transitions {q0 {a q1}
+                          q1 {b q2}
+                          q2 {c q3}}})
+(def dfa2 '{:states #{q0 q1 q2 q3 q4 q5 q6 q7}
+            :alphabet #{e h i l o y}
+            :start q0
+            :accepts #{q2 q4 q7}
+            :transitions {q0 {h q1}
                              q1 {i q2, e q3}
                              q3 {l q5, y q4}
                              q5 {l q6}
-                             q6 {o q7}}})))
- (= (set (let [ss "vwxyz"] (for [i ss, j ss, k ss, l ss] (str i j k l))))
-    (set (__ '{:states #{q0 q1 q2 q3 q4}
-               :alphabet #{v w x y z}
-               :start q0
-               :accepts #{q4}
-               :transitions {q0 {v q1, w q1, x q1, y q1, z q1}
+                             q6 {o q7}}})
+(def dfa3 '{:states #{q0 q1 q2 q3 q4}
+            :alphabet #{v w x y z}
+            :start q0
+            :accepts #{q4}
+            :transitions {q0 {v q1, w q1, x q1, y q1, z q1}
                              q1 {v q2, w q2, x q2, y q2, z q2}
                              q2 {v q3, w q3, x q3, y q3, z q3}
-                             q3 {v q4, w q4, x q4, y q4, z q4}}})))
- (let [res (take 2000 (__ '{:states #{q0 q1}
-                            :alphabet #{0 1}
-                            :start q0
-                            :accepts #{q0}
-                            :transitions {q0 {0 q0, 1 q1}
-                                          q1 {0 q1, 1 q0}}}))]
+                             q3 {v q4, w q4, x q4, y q4, z q4}}})
+(def dfa4 '{:states #{q0 q1}
+            :alphabet #{0 1}
+            :start q0
+            :accepts #{q0}
+            :transitions {q0 {0 q0, 1 q1}
+                          q1 {0 q1, 1 q0}}})
+(def dfa5 '{:states #{q0 q1}
+            :alphabet #{n m}
+            :start q0
+            :accepts #{q1}
+            :transitions {q0 {n q0, m q1}}})
+(def dfa6 '{:states #{q0 q1 q2 q3 q4 q5 q6 q7 q8 q9}
+            :alphabet #{i l o m p t}
+            :start q0
+            :accepts #{q5 q8}
+            :transitions {q0 {l q1}
+                          q1 {i q2, o q6}
+                          q2 {m q3}
+                          q3 {i q4}
+                          q4 {t q5}
+                          q6 {o q7}
+                          q7 {p q8}
+                          q8 {l q9}
+                          q9 {o q6}}})
+
+(and
+ (= #{"a" "ab" "abc"}
+    (set (__ dfa1)))
+ (= #{"hi" "hey" "hello"}
+    (set (__ dfa2)))N
+ (= (set (let [ss "vwxyz"] (for [i ss, j ss, k ss, l ss] (str i j k l))))
+    (set (__ dfa3)))
+ (let [res (take 2000 (__ dfa4))]
    (and (every? (partial re-matches #"0*(?:10*10*)*") res)
         (= res (distinct res))))
- (let [res (take 2000 (__ '{:states #{q0 q1}
-                            :alphabet #{n m}
-                            :start q0
-                            :accepts #{q1}
-                            :transitions {q0 {n q0, m q1}}}))]
+ (let [res (take 2000 (__ dfa5))]
    (and (every? (partial re-matches #"n*m") res)
         (= res (distinct res))))
- (let [res (take 2000 (__ '{:states #{q0 q1 q2 q3 q4 q5 q6 q7 q8 q9}
-                            :alphabet #{i l o m p t}
-                            :start q0
-                            :accepts #{q5 q8}
-                            :transitions {q0 {l q1}
-                                          q1 {i q2, o q6}
-                                          q2 {m q3}
-                                          q3 {i q4}
-                                          q4 {t q5}
-                                          q6 {o q7}
-                                          q7 {p q8}
-                                          q8 {l q9}
-                                          q9 {o q6}}}))]
+ (let [res (take 2000 (__ dfa6))]
    (and (every? (partial re-matches #"limit|(?:loop)+") res)
         (= res (distinct res))))
  )
