6cd3241eb75ac754eaf0daae2f73eb4835e81b41
Jim Menard <jim@jimmenard.com>
2012-10-17 18:46:09 -0400

    152 solved, not yet submitted

--- 4clojure.clj
+++ 4clojure.clj
@@ -154,7 +154,7 @@
 
 (def __
   (fn [& bools]
-    (if (and (seq (filter identity bools)) (seq (filter not bools))) true false))
+   (if (and (seq (filter identity bools)) (seq (filter not bools))) true false))
 )
 
 (and
@@ -3168,6 +3168,197 @@
 ;;; ****************************************************************
 
 ;;; ****************************************************************
+;;; http://www.4clojure.com/problem/152
+
+;; A Latin square of order n is an n x n array that contains n different
+;; elements, each occurring exactly once in each row, and exactly once in
+;; each column. For example, among the following arrays only the first one
+;; forms a Latin square:
+;;
+;; A B C    A B C    A B C
+;; B C A    B C A    B D A
+;; C A B    C A C    C A B
+;;
+;; Let V be a vector of such vectors(1) that they may differ in length(2).
+;; We will say that an arrangement of vectors of V in consecutive rows is an
+;; alignment (of vectors) of V if the following conditions are satisfied:
+
+;; All vectors of V are used.
+;; Each row contains just one vector.
+;; The order of V is preserved.
+;; All vectors of maximal length are horizontally aligned each other.
+;; If a vector is not of maximal length then all its elements are aligned
+;;   with elements of some subvector of a vector of maximal length.
+;;
+;; Let L denote a Latin square of order 2 or greater. We will say that L is
+;; included in V or that V includes L iff there exists an alignment of V
+;; such that contains a subsquare that is equal to L. For example, if V
+;; equals [[1 2 3][2 3 1 2 1][3 1 2]] then there are nine alignments of V
+;; (brackets omitted):
+;;
+;; 
+;;        1              2              3
+;;
+;;      1 2 3          1 2 3          1 2 3
+;;  A   2 3 1 2 1    2 3 1 2 1    2 3 1 2 1
+;;      3 1 2        3 1 2        3 1 2
+;;
+;;      1 2 3          1 2 3          1 2 3
+;;  B   2 3 1 2 1    2 3 1 2 1    2 3 1 2 1
+;;        3 1 2        3 1 2        3 1 2
+;;
+;;      1 2 3          1 2 3          1 2 3
+;;  C   2 3 1 2 1    2 3 1 2 1    2 3 1 2 1
+;;          3 1 2        3 1 2        3 1 2
+;;
+;; Alignment A1 contains Latin square [[1 2 3][2 3 1][3 1 2]], alignments
+;; A2, A3, B1, B2, B3 contain no Latin squares, and alignments C1, C2, C3
+;; contain [[2 1][1 2]]. Thus in this case V includes one Latin square of
+;; order 3 and one of order 2 which is included three times.
+;;
+;; Our aim is to implement a function which accepts a vector of vectors V as
+;; an argument, and returns a map which keys and values are integers. Each
+;; key should be the order of a Latin square included in V, and its value a
+;; count of different Latin squares of that order included in V. If V does
+;; not include any Latin squares an empty map should be returned. In the
+;; previous example the correct output of such a function is {3 1, 2 1} and
+;; not {3 1, 2 3}.
+;;
+;; (1) Of course, we can consider sequences instead of vectors. 
+;; (2) Length of a vector is the number of elements in the vector.
+
+(def __
+     (fn [v]
+       (let [num-rows (count v)
+             lengths (map count v)
+             maxlen (apply max lengths)
+             offsets (for [row v] (range (- maxlen (dec (count row)))))] ; all possible offsets
+         (letfn [(alignment ; returns seq of seqs with nils at beginning of rows that are offset
+                  [row-offsets]         ; one offset for each row
+                  (for [i (range num-rows)
+                        :let [row (nth v i)
+                              offset (nth row-offsets i)]]
+                    (concat (take offset (repeat nil)) row (take (- maxlen offset (count row)) (repeat nil)))))
+                 (combis
+                  ;; Given a list of lists of numbers, return a list whose
+                  ;; elements are all the ordered combitations of coll. So
+                  ;; for example turns '((0 1 2) (0) (0 1))) into
+                  ;; '((0 0 0) (0 0 1) (1 0 0) (1 0 1) (2 0 0) (2 0 1)).
+                  [coll]
+                  (letfn [(do-combis
+                           [coll prev]
+                           (if coll
+                             (for [n (first coll)
+                                   :let [prefix (concat prev (list n))]]
+                               (do-combis (next coll) prefix))
+                             prev))]
+                    (partition (count coll) (flatten (do-combis coll '())))))
+                 (alignments
+                  []
+                  (for [row-offset (combis offsets)] (alignment row-offset)))
+                 (square-at
+                  ;; Returns nil if square contains nil or is out of bounds
+                  [alignment row col size]
+                  (let [square (for [r (range row (+ row size))]
+                                 (subvec (vec (nth alignment r)) col (+ col size)))
+                        flattened (flatten square)]
+                    (if (= (count flattened) (count (filter identity flattened))) ; there are no nils
+                      square
+                      nil)))
+                 (latin-square?
+                  [square]
+                  (and
+                   (apply = (map set square)) ; same symbols in each row
+                   (= (count (first square)) (count (set (first square)))) ; no duplicates
+                   (empty? (let [n-rows (count square)
+                                 n-cols (count (first square))]
+                             (for [r0 (range n-rows)
+                                   c0 (range n-cols)
+                                   r1 (range (inc r0) n-rows)
+                                   c1 (range n-cols)
+                                   :let [cell0 (get-in square (list r0 c0))
+                                         cell1 (get-in square (list r1 c1))]
+                                   :when (and (= cell0 cell1)
+                                              (or (= r0 r1)
+                                                  (= c0 c1)))]
+                               :found-dupe)))))]
+           (let [latin-squares (for [alignment (alignments)
+                                     row (range num-rows)
+                                     col (range maxlen)
+                                     size (range 2 (min (inc (- num-rows row)) (inc (- maxlen col))))
+                                     :let [square (square-at alignment row col size)]
+                                     :when (and square (latin-square? (vec square)))]
+                                 square)]
+             (frequencies (map (comp count first) (set latin-squares)))))))
+  )
+
+(and
+ (= (__ '[[A B C D]
+          [A C D B]
+          [B A D C]
+          [D C A B]])
+    {})
+ (= (__ '[[A B C D E F]
+          [B C D E F A]
+          [C D E F A B]
+          [D E F A B C]
+          [E F A B C D]
+          [F A B C D E]])
+    {6 1})
+ (= (__ '[[A B C D]
+          [B A D C]
+          [D C B A]
+          [C D A B]])
+    {4 1, 2 4})
+ (= (__ '[[B D A C B]
+          [D A B C A]
+          [A B C A B]
+          [B C A B C]
+          [A D B C A]])
+    {3 3})
+ (= (__ [  [2 4 6 3]
+           [3 4 6 2]
+           [6 2 4]  ])
+    {})
+ (= (__ [[1]
+         [1 2 1 2]
+         [2 1 2 1]
+         [1 2 1 2]
+         []       ])
+    {2 2})
+ (= (__ [[3 1 2]
+         [1 2 3 1 3 4]
+         [2 3 1 3]    ])
+    {3 1, 2 2})
+ (= (__ [[8 6 7 3 2 5 1 4]
+         [6 8 3 7]
+         [7 3 8 6]
+         [3 7 6 8 1 4 5 2]
+         [1 8 5 2 4]
+         [8 1 2 4 5]])
+    {4 1, 3 1, 2 7})
+ )
+
+(and
+ (= (__ '[[A B C D] [A C D B] [B A D C] [D C A B]])
+    {})
+ (= (__ '[[A B C D E F] [B C D E F A] [C D E F A B] [D E F A B C] [E F A B C D] [F A B C D E]])
+    {6 1})
+ (= (__ '[[A B C D] [B A D C] [D C B A] [C D A B]])
+    {4 1, 2 4})
+ (= (__ '[[B D A C B] [D A B C A] [A B C A B] [B C A B C] [A D B C A]])
+    {3 3})
+ (= (__ [  [2 4 6 3] [3 4 6 2] [6 2 4]  ])
+    {})
+ (= (__ [[1] [1 2 1 2] [2 1 2 1] [1 2 1 2] []       ])
+    {2 2})
+ (= (__ [[3 1 2] [1 2 3 1 3 4] [2 3 1 3]    ])
+    {3 1, 2 2})
+ (= (__ [[8 6 7 3 2 5 1 4] [6 8 3 7] [7 3 8 6] [3 7 6 8 1 4 5 2] [1 8 5 2 4] [8 1 2 4 5]])
+    {4 1, 3 1, 2 7})
+ )
+
+;;; ****************************************************************
 ;;; Unsolved
 ;;; ****************************************************************
 
@@ -3291,146 +3482,3 @@ symbols are in the alphabet #{'a, 'A, 'b, 'B, ...}."
          #{'A 'b 'C 'd}})
    #{#{'B 'D}
      #{'b 'd}}) )
-
-;;; ****************************************************************
-;;; http://www.4clojure.com/problem/152
-
-;; A Latin square of order n is an n x n array that contains n different
-;; elements, each occurring exactly once in each row, and exactly once in
-;; each column. For example, among the following arrays only the first one
-;; forms a Latin square:
-;;
-;; A B C    A B C    A B C
-;; B C A    B C A    B D A
-;; C A B    C A C    C A B
-;;
-;; Let V be a vector of such vectors(1) that they may differ in length(2).
-;; We will say that an arrangement of vectors of V in consecutive rows is an
-;; alignment (of vectors) of V if the following conditions are satisfied:
-
-;; All vectors of V are used.
-;; Each row contains just one vector.
-;; The order of V is preserved.
-;; All vectors of maximal length are horizontally aligned each other.
-;; If a vector is not of maximal length then all its elements are aligned
-;;   with elements of some subvector of a vector of maximal length.
-;;
-;; Let L denote a Latin square of order 2 or greater. We will say that L is
-;; included in V or that V includes L iff there exists an alignment of V
-;; such that contains a subsquare that is equal to L. For example, if V
-;; equals [[1 2 3][2 3 1 2 1][3 1 2]] then there are nine alignments of V
-;; (brackets omitted):
-;;
-;; 
-;;        1              2              3
-;;
-;;      1 2 3          1 2 3          1 2 3
-;;  A   2 3 1 2 1    2 3 1 2 1    2 3 1 2 1
-;;      3 1 2        3 1 2        3 1 2
-;;
-;;      1 2 3          1 2 3          1 2 3
-;;  B   2 3 1 2 1    2 3 1 2 1    2 3 1 2 1
-;;        3 1 2        3 1 2        3 1 2
-;;
-;;      1 2 3          1 2 3          1 2 3
-;;  C   2 3 1 2 1    2 3 1 2 1    2 3 1 2 1
-;;          3 1 2        3 1 2        3 1 2
-;;
-;; Alignment A1 contains Latin square [[1 2 3][2 3 1][3 1 2]], alignments
-;; A2, A3, B1, B2, B3 contain no Latin squares, and alignments C1, C2, C3
-;; contain [[2 1][1 2]]. Thus in this case V includes one Latin square of
-;; order 3 and one of order 2 which is included three times.
-;;
-;; Our aim is to implement a function which accepts a vector of vectors V as
-;; an argument, and returns a map which keys and values are integers. Each
-;; key should be the order of a Latin square included in V, and its value a
-;; count of different Latin squares of that order included in V. If V does
-;; not include any Latin squares an empty map should be returned. In the
-;; previous example the correct output of such a function is {3 1, 2 1} and
-;; not {3 1, 2 3}.
-;;
-;; (1) Of course, we can consider sequences instead of vectors. 
-;; (2) Length of a vector is the number of elements in the vector.
-
-(def __
-     (fn [v]
-       (let [num-rows (count v)
-             lengths (map count v)
-             maxlen (apply max lengths)
-             offsets (for [row v] (range (- maxlen (dec (count row)))))] ; all possible offsets
-         (letfn [(alignment ; returns seq of seqs with nils at beginning of rows that are offset
-                  [row-offsets]         ; one offset for each row
-                  (for [i (range num-rows)
-                        :let [row (nth v i)
-                              offset (nth row-offsets i)]]
-                    (concat (take offset (repeat nil)) row)))
-                 (combis
-                  ;; Given a list of lists of numbers, return a list whose
-                  ;; elements are all the ordered combitations of coll. So
-                  ;; for example turns '((0 1 2) (0) (0 1))) into
-                  ;; '((0 0 0) (0 0 1) (1 0 0) (1 0 1) (2 0 0) (2 0 1)).
-                  [coll]
-                  (letfn [(do-combis
-                           [coll prev]
-                           (if coll
-                             (for [n (first coll)
-                                   :let [prefix (concat prev (list n))]]
-                               (do-combis (next coll) prefix))
-                             prev))]
-                    (partition (count coll) (flatten (do-combis coll '())))))
-                 (alignments
-                  []
-                  (for [row-offset (combis offsets)] (alignment row-offset)))]
-           (for [as (alignments)]
-             ;; Count Latin squares and add to set of counts
-             )
-         ))
-    )
-  )
-
-(and
- (= (__ '[[A B C D]
-          [A C D B]
-          [B A D C]
-          [D C A B]])
-    {})
- (= (__ '[[A B C D E F]
-          [B C D E F A]
-          [C D E F A B]
-          [D E F A B C]
-          [E F A B C D]
-          [F A B C D E]])
-    {6 1})
- (= (__ '[[A B C D]
-          [B A D C]
-          [D C B A]
-          [C D A B]])
-    {4 1, 2 4})
- (= (__ '[[B D A C B]
-          [D A B C A]
-          [A B C A B]
-          [B C A B C]
-          [A D B C A]])
-    {3 3})
- (= (__ [  [2 4 6 3]
-           [3 4 6 2]
-           [6 2 4]  ])
-    {})
- (= (__ [[1]
-         [1 2 1 2]
-         [2 1 2 1]
-         [1 2 1 2]
-         []       ])
-    {2 2})
- (= (__ [[3 1 2]
-         [1 2 3 1 3 4]
-         [2 3 1 3]    ])
-    {3 1, 2 2})
- (= (__ [[8 6 7 3 2 5 1 4]
-         [6 8 3 7]
-         [7 3 8 6]
-         [3 7 6 8 1 4 5 2]
-         [1 8 5 2 4]
-         [8 1 2 4 5]])
-    {4 1, 3 1, 2 7})
- )
