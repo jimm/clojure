d5df158af64f162c2c7afe0c3c455a4de325a97d
Jim Menard <jim@jimmenard.com>
2012-11-10 20:30:24 -0500

    more testing

--- overtone-tutorial/testing.clj
+++ overtone-tutorial/testing.clj
@@ -26,3 +26,56 @@
 (quux)
 (ctl quux :freq 660)
 (kill quux)
+
+(definst trem [freq 440 depth 10 rate 6 length 3]
+    (* 0.3
+       (line:kr 0 1 length FREE)
+       (saw (+ freq (* depth (sin-osc:kr rate))))))
+
+(trem)
+(kill trem)
+
+(trem 200 60 0.8)
+(kill trem)
+
+(trem 60 30 0.2)
+(kill trem)
+
+(ctl trem)
+
+(:name trem)
+(:params trem)
+(map :name (:params trem))
+
+;;; ****************************************************************
+;;; multiple channels
+
+(defsynth sin-square [freq 440] 
+  (out 0 (* 0.5 (+ (square (* 0.5 freq)) (sin-osc freq))))
+  (out 1 (* 0.5 (+ (square (* 0.5 freq)) (sin-osc freq)))))
+
+(sin-square)
+(stop)
+
+(defsynth sin-square [freq 440] 
+  (out 0 (* [0.5 0.5] (+ (square (* 0.5 freq)) (sin-osc freq)))))
+
+(sin-square [440 443])                  ; works in docs, does not work here (illegal arg)
+(stop)
+
+(defsynth sin-square2 [freq 440] 
+  (out 0 (* 0.5 [(square (* 0.5 freq)) (sin-osc freq)])))
+
+(sin-square2 440)
+(stop)
+
+;;; ****************************************************************
+;;; MIDI
+
+;; doesn't work?
+
+(println (midi-in))
+(midi-out)
+
+(event-debug-on)
+(event-debug-off)
