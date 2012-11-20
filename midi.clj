5ffbd1f737b267866020fe98b15e1aeb11106a33
Jim Menard <jim@jimmenard.com>
2012-11-19 20:44:28 -0500

    example of reading sequence using javax.sound.midi

--- midi.clj
+++ midi.clj
@@ -2,3 +2,6 @@
   (:import (javax.sound.midi MidiSystem)))
 
 (MidiSystem/getReceiver)
+
+(def song (javax.sound.midi.MidiSystem/getSequence (File. "/Users/jimm/src/github/clojure/overtone-tutorial/midi/data/at_sea.midi")))
+(map #(.size %) (.getTracks song))
