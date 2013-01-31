e4543cf015c99f5f7680a17f030b7eb58fc8250a
Jim Menard <jim@jimmenard.com>
2013-01-31 17:20:27 -0500

    Finally, 152 (Latin Squares) is fast enough.

--- 4clojure.clj
+++ 4clojure.clj
@@ -3164,135 +3164,6 @@
 )
 
 ;;; ****************************************************************
-;;; Solved, not yet submitted
-;;; ****************************************************************
-
-;;; ****************************************************************
-;;; Unsolved
-;;; ****************************************************************
-
-;;; ****************************************************************
-;;; http://www.4clojure.com/problem/140
-
-(defn char-to-power-of-2 [c num-bits]
-  (let [diff (- (int c) (int \A))
-        pos (dec (- num-bits diff))]
-  (int (Math/pow 2 pos))))
-
-(defn sym-to-power-of-2 [sym num-bits]
-  (let [c (first (str sym))]
-    (if (Character/isUpperCase c)
-      (char-to-power-of-2 c num-bits)
-      0)))
-
-(defn func-input-value [s]
-  "Given a set of input symbols, return the single value that corresponds to
-1 bits where symbols are true and 0 bits where symbols are false. Assumes
-symbols are in the alphabet #{'a, 'A, 'b, 'B, ...}."
-  (let [num-bits (count s)]
-    (reduce + (map #(sym-to-power-of-2 % num-bits) s))))
-
-(def gray-codes {2 [0 1 3 2], 3 [0 1 3 2 6 7 5 4], 4 [0 1 3 2 6 7 5 4 12 13 15 14 10 11 9 8]})
-
-;; (defn k-map [ss]
-;;   (let [num-bits (count ss)
-;;         gray-code (get gray-codes num-bits)
-;;         true-inputs (map func-input-value ss)
-;;         k-map 
-;;     ;; 
-
-(def __
-  (fn [s]
-    )
-  )
-
-(and
- ;; Create a function which accepts as input a boolean algebra function in
- ;; the form of a set of sets, where the inner sets are collections of
- ;; symbols corresponding to the input boolean variables which satisfy the
- ;; function (the inputs of the inner sets are conjoint, and the sets
- ;; themselves are disjoint... also known as canonical minterms). Note:
- ;; capitalized symbols represent truth, and lower-case symbols represent
- ;; negation of the inputs. Your function must return the minimal function
- ;; which is logically equivalent to the input.
- ;;
- ;; PS --- You may want to give this a read before proceeding: <a
- ;; href="http://en.wikipedia.org/wiki/K_map">K-Maps</a>
- ;;
- ;; PPS --- If you're interested in logic programming more generally, you
- ;; should also check out: <a
- ;; href="https://github.com/clojure/core.logic">core.logic</a>
- (= (__ #{#{'a 'B 'C 'd}
-         #{'A 'b 'c 'd}
-         #{'A 'b 'c 'D}
-         #{'A 'b 'C 'd}
-         #{'A 'b 'C 'D}
-         #{'A 'B 'c 'd}
-         #{'A 'B 'c 'D}
-         #{'A 'B 'C 'd}})
-   #{#{'A 'c} 
-     #{'A 'b}
-     #{'B 'C 'd}})
- (= (__ #{#{'A 'B 'C 'D}
-         #{'A 'B 'C 'd}})
-   #{#{'A 'B 'C}})
- (= (__ #{#{'a 'b 'c 'd}
-         #{'a 'B 'c 'd}
-         #{'a 'b 'c 'D}
-         #{'a 'B 'c 'D}
-         #{'A 'B 'C 'd}
-         #{'A 'B 'C 'D}
-         #{'A 'b 'C 'd}
-         #{'A 'b 'C 'D}})
-   #{#{'a 'c}
-     #{'A 'C}})
- (= (__ #{#{'a 'b 'c} 
-         #{'a 'B 'c}
-         #{'a 'b 'C}
-         #{'a 'B 'C}})
-   #{#{'a}})
- (= (__ #{#{'a 'B 'c 'd}
-         #{'A 'B 'c 'D}
-         #{'A 'b 'C 'D}
-         #{'a 'b 'c 'D}
-         #{'a 'B 'C 'D}
-         #{'A 'B 'C 'd}})
-   #{#{'a 'B 'c 'd}
-     #{'A 'B 'c 'D}
-     #{'A 'b 'C 'D}
-     #{'a 'b 'c 'D}
-     #{'a 'B 'C 'D}
-     #{'A 'B 'C 'd}})
- (= (__ #{#{'a 'b 'c 'd}
-         #{'a 'B 'c 'd}
-         #{'A 'B 'c 'd}
-         #{'a 'b 'c 'D}
-         #{'a 'B 'c 'D}
-         #{'A 'B 'c 'D}})
-   #{#{'a 'c}
-     #{'B 'c}})
- (= (__ #{#{'a 'B 'c 'd}
-         #{'A 'B 'c 'd}
-         #{'a 'b 'c 'D}
-         #{'a 'b 'C 'D}
-         #{'A 'b 'c 'D}
-         #{'A 'b 'C 'D}
-         #{'a 'B 'C 'd}
-         #{'A 'B 'C 'd}})
-   #{#{'B 'd}
-     #{'b 'D}})
- (= (__ #{#{'a 'b 'c 'd}
-         #{'A 'b 'c 'd}
-         #{'a 'B 'c 'D}
-         #{'A 'B 'c 'D}
-         #{'a 'B 'C 'D}
-         #{'A 'B 'C 'D}
-         #{'a 'b 'C 'd}
-         #{'A 'b 'C 'd}})
-   #{#{'B 'D}
-     #{'b 'd}}) )
-
-;;; ****************************************************************
 ;;; http://www.4clojure.com/problem/152
 
 ;; A Latin square of order n is an n x n array that contains n different
@@ -3351,21 +3222,6 @@ symbols are in the alphabet #{'a, 'A, 'b, 'B, ...}."
 ;;
 ;; (1) Of course, we can consider sequences instead of vectors. 
 ;; (2) Length of a vector is the number of elements in the vector.
-
-;;; The solution below is correct but too slow. The last example times out
-;;; on 4clojure.com (they used to all time out; perhaps the test server has
-;;; increased the timeout time.)
-;;;
-;;; Idea: if only moving one row then that doesn't affect any squares that
-;;; don't have that row in it already. Cache answers where map == row
-;;; [starts, offsets].
-
-;;; This is the expanded version. The submittable version is
-;;; below.
-
-;; For debugging only
-(def v [[8 6 7 3 2 5 1 4] [6 8 3 7] [7 3 8 6] [3 7 6 8 1 4 5 2] [1 8 5 2 4] [8 1 2 4 5]])
-(time (__ v))
 
 (def __
   (fn [v]
@@ -3513,3 +3369,132 @@ symbols are in the alphabet #{'a, 'A, 'b, 'B, ...}."
     {4 1, 3 1, 2 7})
  )
 ))
+
+;;; ****************************************************************
+;;; Solved, not yet submitted
+;;; ****************************************************************
+
+;;; ****************************************************************
+;;; Unsolved
+;;; ****************************************************************
+
+;;; ****************************************************************
+;;; http://www.4clojure.com/problem/140
+
+(defn char-to-power-of-2 [c num-bits]
+  (let [diff (- (int c) (int \A))
+        pos (dec (- num-bits diff))]
+  (int (Math/pow 2 pos))))
+
+(defn sym-to-power-of-2 [sym num-bits]
+  (let [c (first (str sym))]
+    (if (Character/isUpperCase c)
+      (char-to-power-of-2 c num-bits)
+      0)))
+
+(defn func-input-value [s]
+  "Given a set of input symbols, return the single value that corresponds to
+1 bits where symbols are true and 0 bits where symbols are false. Assumes
+symbols are in the alphabet #{'a, 'A, 'b, 'B, ...}."
+  (let [num-bits (count s)]
+    (reduce + (map #(sym-to-power-of-2 % num-bits) s))))
+
+(def gray-codes {2 [0 1 3 2], 3 [0 1 3 2 6 7 5 4], 4 [0 1 3 2 6 7 5 4 12 13 15 14 10 11 9 8]})
+
+;; (defn k-map [ss]
+;;   (let [num-bits (count ss)
+;;         gray-code (get gray-codes num-bits)
+;;         true-inputs (map func-input-value ss)
+;;         k-map 
+;;     ;; 
+
+(def __
+  (fn [s]
+    )
+  )
+
+(and
+ ;; Create a function which accepts as input a boolean algebra function in
+ ;; the form of a set of sets, where the inner sets are collections of
+ ;; symbols corresponding to the input boolean variables which satisfy the
+ ;; function (the inputs of the inner sets are conjoint, and the sets
+ ;; themselves are disjoint... also known as canonical minterms). Note:
+ ;; capitalized symbols represent truth, and lower-case symbols represent
+ ;; negation of the inputs. Your function must return the minimal function
+ ;; which is logically equivalent to the input.
+ ;;
+ ;; PS --- You may want to give this a read before proceeding: <a
+ ;; href="http://en.wikipedia.org/wiki/K_map">K-Maps</a>
+ ;;
+ ;; PPS --- If you're interested in logic programming more generally, you
+ ;; should also check out: <a
+ ;; href="https://github.com/clojure/core.logic">core.logic</a>
+ (= (__ #{#{'a 'B 'C 'd}
+         #{'A 'b 'c 'd}
+         #{'A 'b 'c 'D}
+         #{'A 'b 'C 'd}
+         #{'A 'b 'C 'D}
+         #{'A 'B 'c 'd}
+         #{'A 'B 'c 'D}
+         #{'A 'B 'C 'd}})
+   #{#{'A 'c} 
+     #{'A 'b}
+     #{'B 'C 'd}})
+ (= (__ #{#{'A 'B 'C 'D}
+         #{'A 'B 'C 'd}})
+   #{#{'A 'B 'C}})
+ (= (__ #{#{'a 'b 'c 'd}
+         #{'a 'B 'c 'd}
+         #{'a 'b 'c 'D}
+         #{'a 'B 'c 'D}
+         #{'A 'B 'C 'd}
+         #{'A 'B 'C 'D}
+         #{'A 'b 'C 'd}
+         #{'A 'b 'C 'D}})
+   #{#{'a 'c}
+     #{'A 'C}})
+ (= (__ #{#{'a 'b 'c} 
+         #{'a 'B 'c}
+         #{'a 'b 'C}
+         #{'a 'B 'C}})
+   #{#{'a}})
+ (= (__ #{#{'a 'B 'c 'd}
+         #{'A 'B 'c 'D}
+         #{'A 'b 'C 'D}
+         #{'a 'b 'c 'D}
+         #{'a 'B 'C 'D}
+         #{'A 'B 'C 'd}})
+   #{#{'a 'B 'c 'd}
+     #{'A 'B 'c 'D}
+     #{'A 'b 'C 'D}
+     #{'a 'b 'c 'D}
+     #{'a 'B 'C 'D}
+     #{'A 'B 'C 'd}})
+ (= (__ #{#{'a 'b 'c 'd}
+         #{'a 'B 'c 'd}
+         #{'A 'B 'c 'd}
+         #{'a 'b 'c 'D}
+         #{'a 'B 'c 'D}
+         #{'A 'B 'c 'D}})
+   #{#{'a 'c}
+     #{'B 'c}})
+ (= (__ #{#{'a 'B 'c 'd}
+         #{'A 'B 'c 'd}
+         #{'a 'b 'c 'D}
+         #{'a 'b 'C 'D}
+         #{'A 'b 'c 'D}
+         #{'A 'b 'C 'D}
+         #{'a 'B 'C 'd}
+         #{'A 'B 'C 'd}})
+   #{#{'B 'd}
+     #{'b 'D}})
+ (= (__ #{#{'a 'b 'c 'd}
+         #{'A 'b 'c 'd}
+         #{'a 'B 'c 'D}
+         #{'A 'B 'c 'D}
+         #{'a 'B 'C 'D}
+         #{'A 'B 'C 'D}
+         #{'a 'b 'C 'd}
+         #{'A 'b 'C 'd}})
+   #{#{'B 'D}
+     #{'b 'd}}) )
