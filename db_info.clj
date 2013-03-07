8d2bd626e8fb0206563a167a15ecaa703d654da1
Jim Menard <jim@jimmenard.com>
2013-03-07 13:28:10 -0500

    comment

--- db_info.clj
+++ db_info.clj
@@ -1,6 +1,12 @@
 (ns db-info
   (:use [clojure.contrib.sql :as sql]))
 
+;;; This database doesn't exist anymore, and besides the db name, user name,
+;;; and password below were only for dev mode. All three were completely
+;;; different on staging and production.
+;;;
+;;; That said, see drop_tables.clj for how to read those values from the
+;;; coomand line.
 (def db {:classname "com.mysql.jdbc.Driver"
          :subprotocol "mysql"
          :subname "//localhost/brain_development"
