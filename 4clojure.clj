34f89ea154b195ca0077db2c89c74becb7dc0840
Jim Menard <jim@jimmenard.com>
2013-07-19 15:21:42 -0400

    Solution to 178

--- 4clojure.clj
+++ 4clojure.clj
@@ -3411,6 +3411,79 @@
  )
 
 ;;; ****************************************************************
+;;; http://www.4clojure.com/problem/178
+;;;
+;;; Following on from Recognize Playing Cards (#128), determine the best
+;;; poker hand that can be made with five cards. The hand rankings are
+;;; listed below for your convenience.
+;;;
+;;; - Straight flush: All cards in the same suit, and in sequence
+;;; - Four of a kind: Four of the cards have the same rank
+;;; - Full House: Three cards of one rank, the other two of another rank
+;;; - Flush: All cards in the same suit
+;;; - Straight: All cards in sequence (aces can be high or low, but not both at once)
+;;; - Three of a kind: Three of the cards have the same rank
+;;; - Two pair: Two pairs of cards have the same rank
+;;; - Pair: Two cards have the same rank
+;;; - High card: None of the above conditions are met
+
+(def __
+  (fn [cards]
+    (let [suit (fn [s] ({\D :diamond, \S :spade, \H :heart, \C :club} (first s)))
+          rank (fn [s] ({\2 0, \3 1, \4 2, \5 3, \6 4, \7 5, \8 6, \9 7, \T 8, \J 9, \Q 10, \K 11, \A 12} (second s)))
+          suits (map suit cards)
+          ranks (map rank cards)
+          rank-freqs (vals (frequencies ranks))]
+      ;; (print "suits" suits "ranks" ranks "rank-freqs" rank-freqs) ; DEBUG
+      (cond (and (= 1 (count (set suits)))
+                 (= 4 (- (apply max ranks) (apply min ranks))))
+            :straight-flush
+
+            (some #{4} rank-freqs)
+            :four-of-a-kind
+
+            (and (some #{3} rank-freqs)
+                 (some #{2} rank-freqs))
+            :full-house
+
+            (= 1 (count (set suits)))
+            :flush
+
+            ;; TODO aces must be able to be counted as low, too
+            (let [min-rank (apply min ranks)
+                  max-rank (apply max ranks)]
+              (or (= (set (range min-rank (+ min-rank 5))) (set ranks))
+                  (and (= max-rank 12)
+                       (= #{12 0 1 2 3} (set ranks)))))
+            :straight
+
+            (some #{3} rank-freqs)
+            :three-of-a-kind
+
+            (some #{2} (vals (frequencies rank-freqs)))
+            :two-pair
+
+            (some #{2} rank-freqs)
+            :pair
+
+            :else
+            :high-card)))
+  )
+
+(and
+  (= :high-card (__ ["HA" "D2" "H3" "C9" "DJ"]))
+  (= :pair (__ ["HA" "HQ" "SJ" "DA" "HT"]))
+  (= :two-pair (__ ["HA" "DA" "HQ" "SQ" "HT"]))
+  (= :three-of-a-kind (__ ["HA" "DA" "CA" "HJ" "HT"]))
+  (= :straight (__ ["HA" "DK" "HQ" "HJ" "HT"]))
+  (= :straight (__ ["HA" "H2" "S3" "D4" "C5"]))
+  (= :flush (__ ["HA" "HK" "H2" "H4" "HT"]))
+  (= :full-house (__ ["HA" "DA" "CA" "HJ" "DJ"]))
+  (= :four-of-a-kind (__ ["HA" "DA" "CA" "SA" "DJ"]))
+  (= :straight-flush (__ ["HA" "HK" "HQ" "HJ" "HT"]))
+ )
+
+;;; ****************************************************************
 ;;; Solved, not yet submitted
 ;;; ****************************************************************
 
