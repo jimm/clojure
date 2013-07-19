df2f1b93ee9524ca61d89478f15e81b4002f30be
Jim Menard <jim@jimmenard.com>
2013-07-19 14:32:32 -0400

    Solution to 177

--- 4clojure.clj
+++ 4clojure.clj
@@ -3371,6 +3371,46 @@
 ))
 
 ;;; ****************************************************************
+;;; http://www.4clojure.com/problem/177
+;;;
+;;; When parsing a snippet of code it's often a good idea to do a sanity
+;;; check to see if all the brackets match up. Write a function that takes
+;;; in a string and returns truthy if all square [ ] round ( ) and curly { }
+;;; brackets are properly paired and legally nested, or returns falsey
+;;; otherwise.
+
+(def __
+  (fn [s]
+    (let [pairs { \[ \] \] \[ \{ \} \} \{ \( \) \) \( }
+          openers #{ \[ \{ \( }
+          closers (map #(get pairs %) openers)]
+      (loop [s s
+             stack ()]
+        (let [c (first s)]
+          (cond (not s) (empty? stack)
+                (some #{c} openers) (recur (next s) (conj stack (get pairs c)))
+                (some #{c} closers) (if (= c (first stack))
+                                      (recur (next s) (rest stack))
+                                      (recur (next s) (conj stack c))) ; now stack will never be empty
+                :else (recur (next s) stack))))))
+  )
+
+(and
+ (__ "This string has no brackets.")
+ (__ "class Test {
+      public static void main(String[] args) {
+        System.out.println(\"Hello world.\");
+      }
+    }")
+ (not (__ "(start, end]"))
+ (not (__ "())"))
+ (not (__ "[ { ] } "))
+ (__ "([]([(()){()}(()(()))(([[]]({}()))())]((((()()))))))")
+ (not (__ "([]([(()){()}(()(()))(([[]]({}([)))())]((((()()))))))"))
+ (not (__ "["))
+ )
+
+;;; ****************************************************************
 ;;; Solved, not yet submitted
 ;;; ****************************************************************
 
