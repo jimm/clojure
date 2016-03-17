c53976d3dae06cddceab30ca3ff94bc97f57937d
Jim Menard <jim@jimmenard.com>
2016-03-17 09:16:35 -0400

    update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject jimm-clojure "0.1.0-SNAPSHOT"
   :description "Jim's Clojure experimentation"
   :url "https://github.com/jimm/clojure"
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
