551e32de0e4609e3b77456a86945a3a17781d63c
Jim Menard <jim.menard@chloeandisabel.com>
2017-09-20 08:58:24 -0400

    Graphics context map. Makefile.

--- clojurescript/graphics/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-HERE=$(cd $(dirname $0) && pwd)
-java -cp $HOME/lib/cljs.jar:src clojure.main build.clj
--- clojurescript/graphics/src/graphics/core.cljs
+++ clojurescript/graphics/src/graphics/core.cljs
@@ -2,44 +2,57 @@
 
 (ns graphics.core)
 
-(enable-console-print!)
+;; (enable-console-print!)
 
-(defn init [scene camera]
-  (set! (-> camera .-position .-z) 500)
-  (.add scene camera)
+(defn make-scene [] (THREE.Scene.))
 
+(defn make-camera []
+  (THREE.PerspectiveCamera. 75 (/ (.-innerWidth js/window) (.-innerHeight js/window)) 1 1000))
+
+(defn make-renderer [] (THREE.CanvasRenderer.))
+
+(defn place-camera [context]
+  (set! (-> context :camera .-position .-z) 500)
+  (.add (:scene context) (:camera context)))
+
+(defn build-scene [scene]
   (let [geometry (THREE.IcosahedronGeometry. 200 1)
         material (THREE.MeshBasicMaterial. #js {:color 0x000000
                                                 :wireframe true
                                                 :wireframeLinewidth 2})
-        mesh (THREE.Mesh. geometry material)
-        renderer (THREE.CanvasRenderer.)]
+        mesh (THREE.Mesh. geometry material)]
 
     (.add scene mesh)
+    mesh))
+
+(defn make-objs [context]
+  (place-camera context)
+  (let [scene-objs (build-scene (:scene context))
+        renderer (:renderer context)]
     (.setClearColorHex renderer 0xffffff)
     (.setSize renderer window.innerWidth window.innerHeight)
-
     (let [body (.-body js/document)
           style (.-style body)]
       (set! (.-margin style) 0)
       (set! (.-overflow style) "hidden")
       (.appendChild body (.-domElement renderer)))
+    scene-objs))
 
-    [mesh renderer]))
-
-(defn animate [scene camera mesh renderer]
-  (let [rot (.-rotation mesh)
+(defn animate [context scene-objs]
+  ;; scene-objs is a single mesh in this example
+  (let [rot (.-rotation scene-objs)
         d (.now js/Date)]
     (set! (.-x rot) (* d 0.0005))
     (set! (.-y rot) (* d 0.001))
-    (.render renderer scene camera)))
+    (.render (:renderer context) (:scene context) (:camera context))))
 
 (defn main []
-  (let [scene (THREE.Scene.)
-        camera (THREE.PerspectiveCamera. 75 (/ (.-innerWidth js/window) (.-innerHeight js/window)) 1 1000)
-        [mesh renderer] (init scene camera)
+  (let [context {:scene (make-scene)
+                 :camera (make-camera)
+                 :renderer (make-renderer)}
+        scene-objs (make-objs context)
         animation (fn [f]
-                    (animate scene camera mesh renderer)
+                    (animate context scene-objs)
                     (js/requestAnimationFrame #(f f)))]
     (animation animation)))
 
